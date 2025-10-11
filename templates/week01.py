465c4a64e05e1405b36edb27fabf0f1403a1c66c
DavPe <pesek.david0@gmail.com>
2025-10-11 11:03:39 +0200

    add hw2

--- templates/week01.py
+++ templates/week01.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 
-
 def evaluate_general_sum(
     row_matrix: np.ndarray,
     col_matrix: np.ndarray,
@@ -27,8 +26,12 @@ def evaluate_general_sum(
     np.ndarray
         A vector of expected utilities of the players
     """
-
-    raise NotImplementedError
+    # first player is row, second is col
+    first_player = row_strategy @ row_matrix @ col_strategy
+    second_player = row_strategy @ col_matrix @ col_strategy
+    exp_utils = np.array([first_player, second_player])
+    
+    return exp_utils
 
 
 def evaluate_zero_sum(
@@ -50,8 +53,10 @@ def evaluate_zero_sum(
     np.ndarray
         A vector of expected utilities of the players
     """
-
-    raise NotImplementedError
+    first_player = row_strategy @ row_matrix @ col_strategy
+    second_player = -first_player
+    exp_utils = np.array([first_player, second_player])
+    return exp_utils
 
 
 def calculate_best_response_against_row(
@@ -71,8 +76,10 @@ def calculate_best_response_against_row(
     np.ndarray
         The column player's best response
     """
-
-    raise NotImplementedError
+    best_response = row_strategy @ col_matrix
+    col_strategy = np.zeros(col_matrix.shape[1])
+    col_strategy[np.argmax(best_response)] = 1  
+    return col_strategy
 
 
 def calculate_best_response_against_col(
@@ -92,8 +99,10 @@ def calculate_best_response_against_col(
     np.ndarray
         The row player's best response
     """
-
-    raise NotImplementedError
+    best_response = row_matrix @ col_strategy
+    row_strategy = np.zeros(row_matrix.shape[0])
+    row_strategy[np.argmax(best_response)] = 1
+    return row_strategy
 
 
 def evaluate_row_against_best_response(
@@ -115,8 +124,10 @@ def evaluate_row_against_best_response(
     np.float32
         The expected utility of the row player
     """
-
-    raise NotImplementedError
+    col_strategy = np.zeros(col_matrix.shape[1])
+    best_response_index = np.argmax(calculate_best_response_against_row(col_matrix, row_strategy))
+    col_strategy[best_response_index] = 1
+    return row_strategy @ row_matrix @ col_strategy
 
 
 def evaluate_col_against_best_response(
@@ -138,8 +149,10 @@ def evaluate_col_against_best_response(
     np.float32
         The expected utility of the column player
     """
-
-    raise NotImplementedError
+    row_strategy = np.zeros(row_matrix.shape[0])
+    best_response_index = np.argmax(calculate_best_response_against_col(row_matrix, col_strategy))
+    row_strategy[best_response_index] = 1
+    return row_strategy @ col_matrix @ col_strategy
 
 
 def find_strictly_dominated_actions(matrix: np.ndarray) -> np.ndarray:
@@ -155,8 +168,14 @@ def find_strictly_dominated_actions(matrix: np.ndarray) -> np.ndarray:
     np.ndarray
         Indices of strictly dominated actions
     """
+    dominated_actions = []
+    for a in range(matrix.shape[0]):
+        for b in range(matrix.shape[0]):
+            if a != b and all(matrix[a, :] < matrix[b, :]):
+                dominated_actions.append(a)
+                break
+    return np.array(dominated_actions)
 
-    raise NotImplementedError
 
 
 def iterated_removal_of_dominated_strategies(
@@ -176,8 +195,31 @@ def iterated_removal_of_dominated_strategies(
     tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
         Four-tuple of reduced row and column payoff matrices, and remaining row and column actions
     """
-
-    raise NotImplementedError
+    row_actions = np.arange(row_matrix.shape[0])
+    col_actions = np.arange(col_matrix.shape[1])
+    while True:
+        changed = False
+        row_dominated_actions = find_strictly_dominated_actions(row_matrix)
+        col_dominated_actions = find_strictly_dominated_actions(col_matrix.T)
+        if (len(row_dominated_actions) != 0):
+            num_rows = row_matrix.shape[0]
+            mask = np.full(num_rows,True)
+            mask[row_dominated_actions] = False
+            row_matrix = row_matrix[mask]
+            col_matrix = col_matrix[mask]
+            row_actions = row_actions[mask]
+            changed = True
+        if (len(col_dominated_actions) != 0):
+            num_cols = col_matrix.shape[1]
+            mask = np.full(num_cols,True)
+            mask[col_dominated_actions] = False
+            row_matrix = row_matrix[:,mask]
+            col_matrix = col_matrix[:,mask]
+            col_actions = col_actions[mask]
+            changed = True
+        if not changed:
+            break
+    return row_matrix, col_matrix, row_actions, col_actions
 
 
 def main() -> None:
